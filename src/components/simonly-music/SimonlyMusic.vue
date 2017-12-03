b20897380fbf521bccb4b991a1f5450f6a033084
jmarti-theinit <jmarti@theinit.com>
2017-12-03 12:22:32 +0100

    remove logs

--- src/components/simonly-music/SimonlyMusic.vue
+++ src/components/simonly-music/SimonlyMusic.vue
@@ -31,22 +31,14 @@
       },
     },
     watch: {
-      track() {
-// eslint-disable-next-line no-console
-        console.log('track changed!');
-      },
       getBackgroundAudioName() {
         const audio = this.$refs.backgroundAudio;
         const isPlaying = audio && audio.currentTime > 0 && !audio.paused && !audio.ended
           && audio.readyState > 2;
         if (isPlaying) {
-// eslint-disable-next-line no-console
-          console.log('paused');
           audio.pause();
         }
         if (audio && audio.load && audio.play) {
-// eslint-disable-next-line no-console
-          console.log('playing back');
           setTimeout(() => {
             audio.currentTime = 0;
             audio.volume = 0.3;
@@ -57,8 +49,6 @@
       },
     },
     mounted() {
-// eslint-disable-next-line no-console
-      console.log('mounted');
       if (this.$refs.backgroundAudio && this.$refs.backgroundAudio.play) {
         this.$refs.backgroundAudio.volume = 0.3;
         this.$refs.backgroundAudio.play();
