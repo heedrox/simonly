7304cfc87088fb28ba8201d483d794a511079bad
jmarti-theinit <jmarti@theinit.com>
2017-12-03 11:10:14 +0100

    refactor keys

--- src/components/simonly-keys/SimonlyKeys.vue
+++ src/components/simonly-keys/SimonlyKeys.vue
@@ -1,28 +1,28 @@
 <template>
     <div class="simonly-keys">
       <simonly-key class="key" :position="1" :externallyPressedKey="simonlyUI && simonlyUI.pressedKey" @keypress="userPressed"
-                   skin="maria" :showRightKey="simonlyUI && simonlyUI.theRightKey">
+                   skin="1" :showRightKey="simonlyUI && simonlyUI.theRightKey">
       </simonly-key>
       <simonly-key class="key" :position="2" :externallyPressedKey="simonlyUI && simonlyUI.pressedKey" @keypress="userPressed"
-                   skin="jone" :showRightKey="simonlyUI && simonlyUI.theRightKey">
+                   skin="2" :showRightKey="simonlyUI && simonlyUI.theRightKey">
       </simonly-key>
       <simonly-key class="key" :position="3" :externallyPressedKey="simonlyUI && simonlyUI.pressedKey" @keypress="userPressed"
-                   skin="ines" :showRightKey="simonlyUI && simonlyUI.theRightKey">
+                   skin="3" :showRightKey="simonlyUI && simonlyUI.theRightKey">
       </simonly-key>
       <simonly-key class="key" :position="4" :externallyPressedKey="simonlyUI && simonlyUI.pressedKey" @keypress="userPressed"
-                   skin="inigo" :showRightKey="simonlyUI && simonlyUI.theRightKey">
+                   skin="4" :showRightKey="simonlyUI && simonlyUI.theRightKey">
       </simonly-key>
       <simonly-key class="key" :position="5" :externallyPressedKey="simonlyUI && simonlyUI.pressedKey" @keypress="userPressed"
-                   skin="olatz" :showRightKey="simonlyUI && simonlyUI.theRightKey">
+                   skin="5" :showRightKey="simonlyUI && simonlyUI.theRightKey">
       </simonly-key>
       <simonly-key class="key" :position="6" :externallyPressedKey="simonlyUI && simonlyUI.pressedKey" @keypress="userPressed"
-                   skin="jorge" :showRightKey="simonlyUI && simonlyUI.theRightKey">
+                   skin="6" :showRightKey="simonlyUI && simonlyUI.theRightKey">
       </simonly-key>
       <simonly-key class="key" :position="7" :externallyPressedKey="simonlyUI && simonlyUI.pressedKey" @keypress="userPressed"
-                   skin="irene" :showRightKey="simonlyUI && simonlyUI.theRightKey">
+                   skin="7" :showRightKey="simonlyUI && simonlyUI.theRightKey">
       </simonly-key>
       <simonly-key class="key" :position="8" :externallyPressedKey="simonlyUI && simonlyUI.pressedKey" @keypress="userPressed"
-                   skin="mariatxiki" :showRightKey="simonlyUI && simonlyUI.theRightKey">
+                   skin="8" :showRightKey="simonlyUI && simonlyUI.theRightKey">
       </simonly-key>
   </div>
 </template>
