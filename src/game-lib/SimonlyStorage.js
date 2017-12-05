d79c07cdcf4e93c12bcc9bf17d243a5b09353144
jmarti-theinit <jmarti@theinit.com>
2017-12-05 22:54:22 +0100

    change storage (missing previous commit!)

--- src/game-lib/SimonlyStorage.js
+++ src/game-lib/SimonlyStorage.js
@@ -1,66 +1,16 @@
-import repeat from '../lib/repeat';
+/* eslint-disable class-methods-use-this */
+export default class SimonlyStorage {
 
-export default class SimonlyGame {
-
-  constructor(simonlyUI) {
-    this.gameInfo = {
-      numTurn: 1,
-      numKeyInTurn: 1,
-      score: 0,
-      currentTurnKeys: [],
-      userEnteredTurnKeys: [],
-      failed: false,
-    };
-    this.simonlyUI = simonlyUI;
+  set(key, value) {
+    window.localStorage.setItem(key, value);
+    return this.get(key);
   }
 
-  addTurnKeys() {
-    const randomNumber = (min, max) => Math.floor((Math.random() * ((max + 1) - min)) + min);
-    const pendingKeys = this.gameInfo.numTurn - this.gameInfo.currentTurnKeys.length;
-
-    const pushRandomNumber = () => this.gameInfo.currentTurnKeys.push(randomNumber(1, 8));
-    repeat(pendingKeys, pushRandomNumber);
+  get(key) {
+    return Promise.resolve(window.localStorage.getItem(key));
   }
 
-  userPressed(pressedKey) {
-    const userEnteredNumKey = this.gameInfo.userEnteredTurnKeys.length;
-    const expectedKey = this.gameInfo.currentTurnKeys[userEnteredNumKey];
-    if (pressedKey === expectedKey) {
-      this.gameInfo.userEnteredTurnKeys.push(pressedKey);
-      this.gameInfo.score = this.gameInfo.score + 1;
-      this.gameInfo.failed = false;
-      this.checkNextTurn();
-    } else {
-      this.gameInfo.failed = true;
-      this.simonlyUI.roundFailed(expectedKey);
-    }
+  clear() {
+    window.localStorage.clear();
   }
-
-  checkNextTurn() {
-    const userKeys = this.gameInfo.userEnteredTurnKeys;
-    const turnKeys = this.gameInfo.currentTurnKeys;
-    const endOfRound = (userKeys.length === turnKeys.length);
-    if (endOfRound) {
-      this.simonlyUI.roundOk(this.gameInfo.numTurn + 1)
-        .then(() => {
-          this.runTurn(this.gameInfo.numTurn + 1);
-        });
-    }
-  }
-
-  runTurn(num) {
-    this.gameInfo.numTurn = num;
-    this.gameInfo.numKeyInTurn = 0;
-    this.gameInfo.userEnteredTurnKeys = [];
-    this.addTurnKeys();
-    this.simonlyUI.showSequence(this.gameInfo.currentTurnKeys);
-  }
-
-  start() {
-    this.gameInfo.failed = false;
-    this.gameInfo.score = 0;
-    this.gameInfo.currentTurnKeys = [];
-    this.runTurn(1);
-  }
-
 }
