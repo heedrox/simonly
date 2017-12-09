c80876ff78286a8a8c272e1bb21c6f04b099c284
jmarti-theinit <jmarti@theinit.com>
2017-12-09 18:14:10 +0100

    add explain view

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,16 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import NewView from '../views/NewView.vue';
+import ExplainView from '../views/ExplainView.vue';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'hash',
   routes: [{
+    path: '/explain',
+    component: ExplainView,
+  }, {
     path: '/new',
     component: NewView,
   }, {
